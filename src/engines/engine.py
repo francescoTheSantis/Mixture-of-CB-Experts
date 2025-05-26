integrate m_licem pyc into m_licem + fix logic based models' metrics computation

src/engines/engine.py
@@ -45,8 +45,10 @@ class Engine(pl.LightningModule):
 
         self.c_names = c_names
         self.y_name = y_name
+        self.num_classes = len(y_name) if len(y_name)>1 else 2
+        self.class_names = y_name if len(y_name)>1 else ['0','1']
 
-        self.task_metric = Task_Accuracy()
+        self.task_metric = Task_Accuracy(logic_reasoning=True)
         self.concept_metric = Concept_Accuracy()
 
         self.csv_log_dir = csv_log_dir
@@ -152,7 +154,13 @@ class Engine(pl.LightningModule):
             self.c_trues = torch.cat(self.c_trues, dim=0)
             self.c_preds = torch.cat(self.c_preds, dim=0)
             self.y_trues = torch.cat(self.y_trues, dim=0)
-            self.y_preds = torch.cat(self.y_preds, dim=0).argmax(-1)
+            if self.num_classes > 2:
+                # If the number of classes is greater than 1, we need to take the argmax
+                self.y_preds = torch.cat(self.y_preds, dim=0).argmax(-1)
+            else:
+                # If the number of classes is 1, we just discretize the predictions
+                # to get the predicted labels.
+                self.y_preds = (torch.cat(self.y_preds, dim=0) > 0.5).long()
 
             # Convert the tensors to pandas dfs
             c_preds = pd.DataFrame(self.c_preds.cpu().numpy(), columns=self.c_names)
@@ -160,10 +168,10 @@ class Engine(pl.LightningModule):
 
             # Create a list of names for the y_preds and y_trues to create 
             # a pandas containing the list of predicted and true labels
-            y_preds = pd.DataFrame(F.one_hot(self.y_preds.cpu(), len(self.y_name)).numpy(), 
-                                   columns=self.y_name)
-            y_trues = pd.DataFrame(F.one_hot(self.y_trues.cpu(), len(self.y_name)).numpy(), 
-                                   columns=self.y_name)
+            y_preds = pd.DataFrame(F.one_hot(self.y_preds.cpu(), self.num_classes).squeeze().numpy(), 
+                                   columns=self.class_names)
+            y_trues = pd.DataFrame(F.one_hot(self.y_trues.long().cpu(), self.num_classes).squeeze().numpy(), 
+                                   columns=self.class_names)
 
             # Store the pandas dfs
             c_preds.to_csv(f"{self.csv_log_dir}/c_preds.csv", index=False)

src/metrics.py
@@ -6,17 +6,20 @@ class Task_Accuracy(Metric):
     """
     Task accuracy metric of the pytorch_lightning model.
     """
-    def __init__(self, dist_sync_on_step=False):
+    def __init__(self, logic_reasoning=False, dist_sync_on_step=False):
         super().__init__(dist_sync_on_step=dist_sync_on_step)
         self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.logic_reasoning = logic_reasoning
 
     def update(self, preds: torch.Tensor, target: torch.Tensor):
         if len(preds.squeeze().shape) > 1:
             preds = torch.argmax(preds, dim=1)
         else:
-            # TODO: DOES NOT CONSIDER DCR/CMR
-            preds = preds.squeeze() > 0. # we dot use an activation function
+            if self.logic_reasoning:
+                preds = preds.squeeze() > 0.5
+            else:
+                preds = preds.squeeze() > 0. 
         target = target.squeeze()
         assert preds.shape == target.shape
         self.correct += torch.sum(preds == target)
@@ -24,7 +27,7 @@ class Task_Accuracy(Metric):
 
     def compute(self):
         return self.correct.float() / self.total
-    
+
 
 class Concept_Accuracy(Metric):
     """

src/models/dcr.py
@@ -91,7 +91,7 @@ class DeepConceptReasoner(BaseModel):
 
     def loss(self, y_hat, y, c_hat=None, c=None):
         # one hot encode the y variable
-        if self.task == 'classification':
+        if self.task == 'classification' and self.output_size > 1:
             y = F.one_hot(y.flatten().long(), num_classes=self.output_size).float()
         loss = self.concept_based_loss(y_hat, y, c_hat, c)
         return loss

src/models/m_licem.py
@@ -22,6 +22,8 @@ class PredictCBM(BaseModel):
                  memory_size=7,
                  weight_reg=1e-4,
                  mc_approx=10,
+                 negative_concepts=False,
+                 hard_concepts=False,
                  ):
 
         super().__init__(
@@ -33,8 +35,7 @@ class PredictCBM(BaseModel):
                  c_groups
                  )
 
-        # Parameters in common with the other Concept Embedding
-        # based models.
+        # Parameters in common with the other Concept Embedding based models.
         self.embedding_size = embedding_size
         self.task_penalty = task_penalty
         self.c_names = list(c_names)
@@ -43,6 +44,8 @@ class PredictCBM(BaseModel):
         self.has_concepts = True
         self.noise = noise
         self.concept_loss_form = nn.BCELoss()
+        self.negative_concepts = negative_concepts
+        self.hard_concepts = hard_concepts
 
         self.bottleneck = pyc_nn.ConceptEmbeddingBottleneck(
             latent_size,
@@ -74,8 +77,20 @@ class PredictCBM(BaseModel):
         )
         c_pred = c_dict['c_int']
 
+        # If hard_concepts is True, convert the predicted concepts to binary values
+        if self.hard_concepts:
+            input_concepts = (c_pred > 0.5).float()
+        else:
+            input_concepts = c_pred
+
+        # If negative_concepts is True, convert the concepts to a range of [-1, 1]
+        if self.negative_concepts:
+            input_concepts = 2 * input_concepts - 1 #TODO: consider converting into convex combination of positive and weights as in CBM
+        else:
+            input_concepts = input_concepts
+
         # Predict a CBM from the memory bank and classify the samples in the batch
-        y_pred, c_pred, predicted_cbm, selection_dist = self.classifier(c_emb, c_pred, self.current_epoch)
+        y_pred, predicted_cbm, selection_dist = self.classifier(c_emb, input_concepts, self.current_epoch)
         return y_pred, c_pred, predicted_cbm, selection_dist
 
     def loss(self, y_hat, y, c_hat=None, c=None):

src/models/modules.py
@@ -66,6 +66,7 @@ class LinearMemoryClassifier(nn.Module):
         # This implies computing the logits of the categorical distribution
         # that will be used to sample the CBM.
         selection = self.selector(c_emb)
+        
         # Store a copy of the selection for metrics
         selection_dist = selection.clone()
         selection = selection.unsqueeze(-1)
@@ -104,7 +105,7 @@ class LinearMemoryClassifier(nn.Module):
         # Add the global biases
         y_probs = y_probs + self.biases.unsqueeze(0).unsqueeze(-1).expand(bsz, -1, n_samples)
 
-        return y_probs, c_pred, predicted_cbm, selection_dist
+        return y_probs, predicted_cbm, selection_dist
     
     def forward(self, c_emb, c_pred, current_epoch=0):
         return self.classify(c_emb, c_pred, current_epoch)

src/trainer.py
@@ -109,7 +109,13 @@ class Trainer:
                     y = torch.cat(y_trues, dim=0)
                     y_preds = torch.cat(y_preds, dim=0)
                     y = y.cpu().numpy()
-                    y_preds = y_preds.argmax(-1).cpu().numpy()
+                    if len(self.cfg.model.params.y_names)==1:
+                        if self.model.model.__class__.__name__ in ['DeepConceptReasoner', 'ConceptMemoryReasoner']:
+                            y_preds = (y_preds > 0.5).long().cpu().numpy()
+                        else:
+                            y_preds = (y_preds > 0.).long().cpu().numpy()
+                    else:
+                        y_preds = y_preds.argmax(-1).cpu().numpy()
                     task_f1, task_acc = f1_acc_metrics(y, y_preds)
                     intervention_results = {'noise': round(eps,1), 'p_int': round(p_int,1), 'f1': round(task_f1,2), 'accuracy': round(task_acc,2)}
                     intervention_df = pd.concat([intervention_df, pd.DataFrame([intervention_results])], ignore_index=True)