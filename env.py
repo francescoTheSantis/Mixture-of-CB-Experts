61582968600623af3e54f4ee9cad95d819a2e974
Gabriele Ciravegna <48250744+gabrieleciravegna@users.noreply.github.com>
2025-07-21 23:27:59 +0200

    changed DATA_PATH setting
    - now it is set from the common.yaml file
    - unable to make it work previously with kaggle datasets

--- env.py
+++ env.py
@@ -2,8 +2,8 @@ from os import environ as env
 from pathlib import Path
 import os
 
-PROJECT_NAME = "linear_memory_reasoner" # Name of the project
-HOME = "" # Path to the project
+PROJECT_NAME = 'linear_memory_reasoner' # Name of the project
+HOME = "/Users/gabrieleciravegna/Code/Linear-Memory-Reasoner" # Path to the project
 
 CACHE = Path(
     env.get(
@@ -15,8 +15,6 @@ CACHE = Path(
     )
 ).expanduser()
 CACHE.mkdir(exist_ok=True)
-
-DATA_PATH = "" # Path to the datasets
 
 env['HYDRA_FULL_ERROR'] = '1'
 
