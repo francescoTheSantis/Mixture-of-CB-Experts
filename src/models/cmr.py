65a26e3ddaaf8af5175238ca5123a5e787e27496
Gabriele Ciravegna <48250744+gabrieleciravegna@users.noreply.github.com>
2025-07-23 11:30:56 +0200

    Adjusting task_penalty for DCR, CMR
    - BCE gives lower task loss values compared to CrossEntropy

--- src/models/cmr.py
+++ src/models/cmr.py
@@ -44,7 +44,7 @@ class ConceptMemoryReasoner(BaseModel):
         
         self.embedding_size = embedding_size
         self.latent_size = latent_size
-        self.task_penalty = task_penalty
+        self.task_penalty = task_penalty * 3 # BCE gives lower loss values
         self.c_names = list(c_names)
         self.int_prob = int_prob
         self.int_idxs = int_idxs
