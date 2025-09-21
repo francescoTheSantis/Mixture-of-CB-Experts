4ed78a04c07601f401c6588d401ea51a4d1b433f
francescoTheSantis <dexo115bis@gmail.com>
2025-09-21 11:03:00 +0200

    updated config

--- show_results.py
+++ show_results.py
@@ -27,11 +27,11 @@ custom_order = [#'xor', \
                 #'cebab',
                 #'celeba',
                 #'cifar10',
-                #'cifar100',
+                'cifar100',
                 'dsprites_simple',
                 'dsprites_complex',
                 'mnist_arithmetic',
-                #'pendulum'
+                'pendulum'
                 ]
 
 # Define a dictionary to associate marker, name, and color to each model.
@@ -72,56 +72,64 @@ def main():
     ###### Visualize training/test distribution over licem's weights #######
     ########################################################################
 
-    # try:
+    try:
 
-    weights_path = "/home/fdesantis/projects/Linear-Memory-Reasoner/test/2025-09-20_21-13-39/0/logs/experiment_metrics/version_0"
+        weights_path = "/home/fdesantis/projects/Linear-Memory-Reasoner/test/2025-09-20_21-13-39/0/logs/experiment_metrics/version_0"
 
-    # Store the training weights
-    train_w = torch.load(weights_path+'/learned_linear_coefficients_train.pt').squeeze(1)
-    #iterate over all the test weights and store them in a list
-    test_weights = {}
-    for file in os.listdir(weights_path):
-        if file.startswith('learned_linear_coefficients_test'):
-            key = file.replace('learned_linear_coefficients_test_', '').replace('.pt', '')
-            test_weights[key] = torch.load(os.path.join(weights_path, file)).squeeze(1)
+        # Store the training weights
+        train_w = torch.load(weights_path+'/learned_linear_coefficients_train.pt').squeeze(1)
+        #iterate over all the test weights and store them in a list
+        test_weights = {}
+        for file in os.listdir(weights_path):
+            if file.startswith('learned_linear_coefficients_test'):
+                key = file.replace('learned_linear_coefficients_test_', '').replace('.pt', '')
+                test_weights[key] = torch.load(os.path.join(weights_path, file)).squeeze(1)
 
-    # read the c_names and y_names
-    with open(weights_path+'/c_names.txt', 'r') as f:
-        c_names = [line.strip() for line in f.readlines()]
-    with open(weights_path+'/y_names.txt', 'r') as f:
-        y_names = [line.strip() for line in f.readlines()]
-    plot_licem_weights_distribution(
-        train_w, 
-        test_weights, 
-        result_figs, 
-        c_names, 
-        y_names,
-        title_font,
-        label_font,
-        tick_font,
-        legend_font
-    )
-    print("Saved LICEM weights distribution plot.")
-    # except Exception as e:
-    #     print(f"Error occurred while plotting LICEM weights distribution: {e}")
+        # read the c_names and y_names
+        with open(weights_path+'/c_names.txt', 'r') as f:
+            c_names = [line.strip() for line in f.readlines()]
+        with open(weights_path+'/y_names.txt', 'r') as f:
+            y_names = [line.strip() for line in f.readlines()]
+
+        plot_licem_weights_distribution(
+            train_w, 
+            test_weights, 
+            result_figs, 
+            c_names, 
+            y_names,
+            title_font,
+            label_font,
+            tick_font,
+            legend_font
+        )
+
+    except Exception as e:
+        print(f"Error occurred while plotting LICEM weights distribution: {e}")
 
 
     ##################################################
     ###### Visualize ablation over the memory. #######
     ##################################################
 
+    # paths = [
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/blackbox/2025-09-13_20-06-02",
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/cem/2025-09-13_20-06-02",
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/cmr/2025-09-13_20-06-02",
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/dcr/2025-09-13_20-06-06",
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/l_cmr/2025-09-13_20-07-01",
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/licem/2025-09-13_20-07-18",
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/sym_cmr_prior/2025-09-13_20-09-06",
+    #     # KANs
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/sym_cmr_kan/2025-09-14_23-29-28",
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/sym_cmr_kan/2025-09-14_23-29-29",
+    #     "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/sym_cmr_kan/2025-09-14_23-29-30",
+    # ]
+
     paths = [
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/blackbox/2025-09-13_20-06-02",
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/cem/2025-09-13_20-06-02",
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/cmr/2025-09-13_20-06-02",
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/dcr/2025-09-13_20-06-06",
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/l_cmr/2025-09-13_20-07-01",
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/licem/2025-09-13_20-07-18",
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/sym_cmr_prior/2025-09-13_20-09-06",
-        # KANs
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/sym_cmr_kan/2025-09-14_23-29-28",
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/sym_cmr_kan/2025-09-14_23-29-29",
-        "/home/fdesantis/projects/Linear-Memory-Reasoner/saved_outputs/sym_cmr_kan/2025-09-14_23-29-30",
+        "/home/fdesantis/projects/Linear-Memory-Reasoner/output/blackbox/2025-09-21_00-30-32",
+        "/home/fdesantis/projects/Linear-Memory-Reasoner/output/cem/2025-09-21_00-30-32",
+        "/home/fdesantis/projects/Linear-Memory-Reasoner/output/l_cmr/2025-09-21_00-32-28",
+        "/home/fdesantis/projects/Linear-Memory-Reasoner/output/sym_cmr_kan/2025-09-21_00-30-32"
     ]
 
     try:
